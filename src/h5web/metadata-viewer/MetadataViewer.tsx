49d8d7bf4bddb66a16ca0471a286e3d8e9e4bb77
Loïc Huder <loic.huder@esrf.fr>
2020-03-04 13:10:55 +0100

    Added basic display of metadata shape and type for base types

--- src/h5web/metadata-viewer/MetadataViewer.tsx
+++ src/h5web/metadata-viewer/MetadataViewer.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { HDF5Link, HDF5Collection } from '../providers/models';
 import styles from './MetadataViewer.module.css';
 import { useMetadata } from '../providers/hooks';
-import { isHardLink } from '../providers/type-guards';
+import {
+  isBaseType,
+  isDataset,
+  isHardLink,
+  isSimpleShape,
+} from '../providers/type-guards';
 
 const ENTITY_TYPE: Record<HDF5Collection, string> = {
   [HDF5Collection.Datasets]: 'dataset',
@@ -25,6 +30,25 @@ function MetadataViewer(props: Props): JSX.Element {
         Metadata for {isHardLink(link) ? ENTITY_TYPE[link.collection] : 'link'}{' '}
         <code>{link.title}</code>
       </h2>
+      {!!metadata && isHardLink(link) && isDataset(metadata, link) && (
+        <table>
+          <tbody>
+            <tr>
+              <th>HDF5 type</th>
+              <td>
+                {isBaseType(metadata.type) ? metadata.type.class : 'Complex'}
+              </td>
+            </tr>
+            <tr>
+              <th>Shape</th>
+              <td>
+                {isSimpleShape(metadata.shape) &&
+                  metadata.shape.dims.toString()}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      )}
       <pre>
         {JSON.stringify(
           metadata,
--- src/h5web/providers/type-guards.ts
+++ src/h5web/providers/type-guards.ts
@@ -1,5 +1,38 @@
-import { HDF5Link, HDF5LinkClass, HDF5HardLink } from './models';
+import {
+  HDF5Collection,
+  HDF5Dataset,
+  HDF5Entity,
+  HDF5HardLink,
+  HDF5Link,
+  HDF5LinkClass,
+  HDF5SimpleShape,
+  HDF5Shape,
+  HDF5ShapeClass,
+  HDF5BaseType,
+  HDF5Type,
+  HDF5TypeClass,
+} from './models';
 
 export function isHardLink(link: HDF5Link): link is HDF5HardLink {
   return link.class === HDF5LinkClass.Hard;
+}
+
+export function isDataset(
+  entity: HDF5Entity,
+  link: HDF5HardLink
+): entity is HDF5Dataset {
+  return link.collection === HDF5Collection.Datasets;
+}
+
+export function isSimpleShape(shape: HDF5Shape): shape is HDF5SimpleShape {
+  return shape.class === HDF5ShapeClass.Simple;
+}
+
+export function isBaseType(type: HDF5Type): type is HDF5BaseType {
+  return (
+    typeof type !== 'string' &&
+    [HDF5TypeClass.Integer, HDF5TypeClass.Float, HDF5TypeClass.String].includes(
+      type.class
+    )
+  );
 }
