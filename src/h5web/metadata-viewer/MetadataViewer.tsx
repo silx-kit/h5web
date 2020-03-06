13bac89dec0f74e61e62808e5bce4b25596d6ddc
Loïc Huder <loic.huder@esrf.fr>
2020-03-06 13:30:30 +0100

    Added pretty display of attributes in its own component

--- src/h5web/metadata-viewer/MetadataViewer.tsx
+++ src/h5web/metadata-viewer/MetadataViewer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { HDF5Link, HDF5Collection } from '../providers/models';
+import { HDF5Collection, HDF5Link } from '../providers/models';
 import styles from './MetadataViewer.module.css';
 import { useEntity } from '../providers/hooks';
 import { isBaseType, isDataset, isHardLink } from '../providers/type-guards';
 import ShapeRenderer from './ShapeRenderer';
+import AttributesRenderer from './AttributesRenderer';
 
 const ENTITY_TYPE: Record<HDF5Collection, string> = {
   [HDF5Collection.Datasets]: 'dataset',
@@ -27,24 +28,33 @@ function MetadataViewer(props: Props): JSX.Element {
         Metadata for {isHardLink(link) ? ENTITY_TYPE[link.collection] : 'link'}{' '}
         <code>{link.title}</code>
       </h2>
-      {entity && isHardLink(link) && isDataset(entity, link) && (
+      {entity && (
         <table>
-          <thead>
-            <tr>
-              <th>Dataset info</th>
-            </tr>
-          </thead>
           <tbody>
-            <tr>
-              <th>HDF5 type</th>
-              <td>{isBaseType(entity.type) ? entity.type.class : 'Complex'}</td>
-            </tr>
-            <tr>
-              <th>Shape</th>
-              <td>
-                <ShapeRenderer shape={entity.shape} />
-              </td>
-            </tr>
+            {isHardLink(link) && isDataset(entity, link) && (
+              <>
+                <tr>
+                  <th className={styles.table_head} colSpan={2}>
+                    Dataset info
+                  </th>
+                </tr>
+                <tr>
+                  <th>HDF5 type</th>
+                  <td>
+                    {isBaseType(entity.type) ? entity.type.class : 'Complex'}
+                  </td>
+                </tr>
+                <tr>
+                  <th>Shape</th>
+                  <td>
+                    <ShapeRenderer shape={entity.shape} />
+                  </td>
+                </tr>
+              </>
+            )}
+            {'attributes' in entity && (
+              <AttributesRenderer attributes={entity.attributes} />
+            )}
           </tbody>
         </table>
       )}
