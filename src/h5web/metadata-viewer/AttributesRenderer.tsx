57c24c296cf55cf1a546f05ab8e7a673a6e4cca1
Loïc Huder <loic.huder@esrf.fr>
2020-03-09 12:55:36 +0100

    Empty attribute lists no longer display the Attributes header

--- src/h5web/metadata-viewer/AttributesRenderer.tsx
+++ src/h5web/metadata-viewer/AttributesRenderer.tsx
@@ -3,11 +3,15 @@ import { HDF5Attribute } from '../providers/models';
 import styles from './MetadataViewer.module.css';
 
 interface Props {
-  attributes: HDF5Attribute[] | undefined;
+  attributes?: HDF5Attribute[];
 }
 
 function AttributesRenderer(props: Props): JSX.Element {
   const { attributes } = props;
+
+  if (!attributes || attributes.length === 0) {
+    return <></>;
+  }
 
   return (
     <>
@@ -16,17 +20,16 @@ function AttributesRenderer(props: Props): JSX.Element {
           Attributes
         </th>
       </tr>
-      {attributes &&
-        attributes.map(
-          (attribute: HDF5Attribute): JSX.Element => {
-            return (
-              <tr>
-                <th>{attribute.name}</th>
-                <td>{attribute.value}</td>
-              </tr>
-            );
-          }
-        )}
+      {attributes.map(
+        (attribute: HDF5Attribute): JSX.Element => {
+          return (
+            <tr>
+              <th>{attribute.name}</th>
+              <td>{attribute.value}</td>
+            </tr>
+          );
+        }
+      )}
     </>
   );
 }
