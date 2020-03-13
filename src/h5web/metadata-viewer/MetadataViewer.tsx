0af4e057779a4bb1155cc8cecf520fa4c0c56f66
Axel Bocciarelli <axel.bocciarelli@esrf.fr>
2020-03-13 10:15:28 +0100

    Pass dataset instead of link to DatasetVisualizer

--- src/h5web/metadata-viewer/MetadataViewer.tsx
+++ src/h5web/metadata-viewer/MetadataViewer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { HDF5Link } from '../providers/models';
-import { useEntity } from '../providers/hooks';
+import { HDF5Link, HDF5GenericEntity } from '../providers/models';
 import AttributesInfo from './AttributesInfo';
 import LinkInfo from './LinkInfo';
 import EntityInfo from './EntityInfo';
@@ -8,12 +7,11 @@ import EntityInfo from './EntityInfo';
 interface Props {
   key: string;
   link: HDF5Link;
+  entity?: HDF5GenericEntity;
 }
 
 function MetadataViewer(props: Props): JSX.Element {
-  const { link } = props;
-
-  const entity = useEntity(link);
+  const { link, entity } = props;
 
   return (
     <>
