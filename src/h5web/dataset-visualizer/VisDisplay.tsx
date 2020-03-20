e8c0458580ade0eb4ec055c4ab47985bc5a4a2b3
Axel Bocciarelli <axelboc@gmail.com>
2020-03-20 12:15:56 +0100

    Implement Scalar Vis

--- src/h5web/dataset-visualizer/VisDisplay.tsx
+++ src/h5web/dataset-visualizer/VisDisplay.tsx
@@ -3,6 +3,7 @@ import { HDF5Value, HDF5SimpleShape, HDF5Dataset } from '../providers/models';
 import { Vis } from './models';
 import RawVis from './vis/RawVis';
 import MatrixVis from './vis/MatrixVis';
+import ScalarVis from './vis/ScalarVis';
 
 interface Props {
   vis: Vis;
@@ -15,6 +16,10 @@ function VisDisplay(props: Props): JSX.Element {
 
   if (vis === Vis.Raw) {
     return <RawVis data={value} />;
+  }
+
+  if (vis === Vis.Scalar) {
+    return <ScalarVis data={value} />;
   }
 
   if (vis === Vis.Matrix) {
---
+++ src/h5web/dataset-visualizer/vis/ScalarVis.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './ScalarVis.module.css';
+import { HDF5Value } from '../../providers/models';
+
+interface Props {
+  data: HDF5Value;
+}
+
+function ScalarVis(props: Props): JSX.Element {
+  const { data } = props;
+  return <div className={styles.scalar}>{data.toString()}</div>;
+}
+
+export default ScalarVis;
