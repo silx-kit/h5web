2f541329fbbd93e63c9354a07d176b597525f644
Axel Bocciarelli <axelboc@gmail.com>
2020-04-07 11:47:03 +0200

    Implement pan (unbounded)

--- src/h5web/visualizations/heatmap/Mesh.tsx
+++ src/h5web/visualizations/heatmap/Mesh.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { useThree, PointerEvent } from 'react-three-fiber';
+import { useThree } from 'react-three-fiber';
 import { RGBFormat, OrthographicCamera } from 'three';
-
-const ZOOM_FACTOR = 0.95;
+import { usePanZoom } from './utils';
 
 interface Props {
   dims: [number, number];
@@ -13,23 +12,13 @@ function Mesh(props: Props): JSX.Element {
   const { dims, textureData } = props;
   const [rows, cols] = dims;
 
-  const { size } = useThree();
+  const { camera, size } = useThree();
   const { width, height } = size;
 
-  function handleWheel(evt: PointerEvent): void {
-    evt.stopPropagation();
-
-    // Fix react-three-fiber typings
-    const camera = evt.camera as OrthographicCamera;
-    const wheelEvt = evt as React.WheelEvent<HTMLDivElement>;
-
-    const factor = wheelEvt.deltaY > 0 ? ZOOM_FACTOR : 1 / ZOOM_FACTOR;
-    camera.zoom = Math.max(1, camera.zoom * factor);
-    camera.updateProjectionMatrix();
-  }
+  const pointerHandlers = usePanZoom(camera as OrthographicCamera);
 
   return (
-    <mesh onWheel={handleWheel}>
+    <mesh {...pointerHandlers}>
       <planeBufferGeometry attach="geometry" args={[width, height]} />
       <meshBasicMaterial attach="material">
         <dataTexture attach="map" args={[textureData, cols, rows, RGBFormat]} />
--- src/h5web/visualizations/heatmap/utils.ts
+++ src/h5web/visualizations/heatmap/utils.ts
@@ -1,7 +1,12 @@
+import React, { useCallback, useRef } from 'react';
 import { extent } from 'd3-array';
 import { rgb } from 'd3-color';
 import { scaleSequential } from 'd3-scale';
 import { interpolateMagma } from 'd3-scale-chromatic';
+import { OrthographicCamera, Vector3 } from 'three';
+import { ReactThreeFiber, PointerEvent } from 'react-three-fiber';
+
+const ZOOM_FACTOR = 0.95;
 
 export function computeTextureData(matrix: number[][]): Uint8Array | undefined {
   const values = matrix.flat();
@@ -22,4 +27,68 @@ export function computeTextureData(matrix: number[][]): Uint8Array | undefined {
   });
 
   return Uint8Array.from(colors.flat());
+}
+
+export function usePanZoom(camera: OrthographicCamera): ReactThreeFiber.Events {
+  const startOffsetPosition = useRef<Vector3>(); // `useRef` to avoid re-renders
+
+  const onPointerDown = useCallback(
+    (evt: PointerEvent) => {
+      evt.stopPropagation();
+
+      const { currentTarget, pointerId } = evt as React.PointerEvent;
+      currentTarget.setPointerCapture(pointerId);
+
+      const projectedPoint = camera.worldToLocal(evt.unprojectedPoint.clone());
+      startOffsetPosition.current = camera.position.clone().add(projectedPoint);
+    },
+    [camera]
+  );
+
+  const onPointerUp = useCallback((evt: PointerEvent) => {
+    evt.stopPropagation();
+
+    const { currentTarget, pointerId } = evt as React.PointerEvent;
+    currentTarget.releasePointerCapture(pointerId);
+
+    startOffsetPosition.current = undefined;
+  }, []);
+
+  const onPointerMove = useCallback(
+    (evt: PointerEvent) => {
+      if (!startOffsetPosition.current) {
+        return;
+      }
+
+      evt.stopPropagation();
+
+      const projectedPoint = camera.worldToLocal(evt.unprojectedPoint.clone());
+      const { x: pointX, y: pointY } = projectedPoint;
+      const { x: startX, y: startY } = startOffsetPosition.current;
+
+      camera.position.set(startX - pointX, startY - pointY, camera.position.z);
+    },
+    [camera, startOffsetPosition]
+  );
+
+  const onWheel = useCallback(
+    (evt: PointerEvent) => {
+      evt.stopPropagation();
+
+      const { deltaY } = evt as React.WheelEvent;
+      const factor = deltaY > 0 ? ZOOM_FACTOR : 1 / ZOOM_FACTOR;
+
+      // eslint-disable-next-line no-param-reassign
+      camera.zoom = Math.max(1, camera.zoom * factor);
+      camera.updateProjectionMatrix();
+    },
+    [camera]
+  );
+
+  return {
+    onPointerDown,
+    onPointerUp,
+    onPointerMove,
+    onWheel,
+  };
 }
