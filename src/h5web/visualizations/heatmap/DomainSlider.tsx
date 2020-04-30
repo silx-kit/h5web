ff4d8e259faa2a791963aa5edd61404dde6048e5
Loïc Huder <loic.huder@esrf.fr>
2020-04-30 15:48:24 +0200

    Added a button to reset colormap limits to data

--- src/h5web/visualizations/heatmap/DomainSlider.tsx
+++ src/h5web/visualizations/heatmap/DomainSlider.tsx
@@ -3,12 +3,13 @@ import ReactSlider from 'react-slider';
 import shallow from 'zustand/shallow';
 import { format } from 'd3-format';
 import { round } from 'lodash-es';
+import { FiRotateCcw } from 'react-icons/fi';
 import styles from './DomainSlider.module.css';
 import { useHeatmapConfig } from './config';
 import { Domain } from './models';
 
 const EXTEND_PERCENTAGE = 0.2;
-const MIN_DECIMALS = 1;
+const NB_DECIMALS = 1;
 
 function DomainSlider(): JSX.Element {
   const [
@@ -30,28 +31,38 @@ function DomainSlider(): JSX.Element {
   const extension = (max - min) * EXTEND_PERCENTAGE;
   const extendedMin = min - extension;
   const extendedMax = max + extension;
-  const step = Math.max((extendedMax - extendedMin) / 100, 10 ** -MIN_DECIMALS);
+  const step = Math.max((extendedMax - extendedMin) / 100, 10 ** -NB_DECIMALS);
 
   return (
-    <ReactSlider
-      className={styles.slider}
-      trackClassName={styles.sliderTrack}
-      thumbClassName={styles.sliderThumb}
-      renderThumb={(props, state) => (
-        <div {...props}>{format(`.${MIN_DECIMALS}f`)(state.valueNow)}</div>
-      )}
-      value={[...(customDomain || dataDomain)]}
-      min={extendedMin}
-      max={extendedMax}
-      step={step}
-      pearling
-      onAfterChange={values => {
-        const roundedDomain = (values as number[]).map(value =>
-          round(value, MIN_DECIMALS)
-        ) as Domain;
-        setCustomDomain(roundedDomain);
-      }}
-    />
+    <div className={styles.sliderWrapper}>
+      <button
+        className={styles.resetButton}
+        type="button"
+        onClick={() => setCustomDomain(undefined)}
+        disabled={!customDomain}
+      >
+        <FiRotateCcw />
+      </button>
+      <ReactSlider
+        className={styles.slider}
+        trackClassName={styles.sliderTrack}
+        thumbClassName={styles.sliderThumb}
+        renderThumb={(props, state) => (
+          <div {...props}>{format(`.${NB_DECIMALS}f`)(state.valueNow)}</div>
+        )}
+        value={[...(customDomain || dataDomain)]}
+        min={extendedMin}
+        max={extendedMax}
+        step={step}
+        pearling
+        onAfterChange={values => {
+          const roundedDomain = (values as number[]).map(value =>
+            round(value, NB_DECIMALS)
+          ) as Domain;
+          setCustomDomain(roundedDomain);
+        }}
+      />
+    </div>
   );
 }
 
