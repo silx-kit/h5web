80f64fec7d9ff472d1ea66627ad7ef4565caf38a
Loïc Huder <loic.huder@esrf.fr>
2020-05-13 09:15:35 +0200

    Refactored Tooltip to be used in Line and Heatmap Vis

--- src/h5web/visualizations/line/LineVis.tsx
+++ src/h5web/visualizations/line/LineVis.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import shallow from 'zustand/shallow';
+import { format } from 'd3-format';
 import styles from './LineVis.module.css';
 import DataLine from './DataLine';
 import VisCanvas from '../shared/VisCanvas';
@@ -7,9 +8,11 @@ import PanZoomMesh from '../shared/PanZoomMesh';
 import { useProps, useAxisDomains } from './hooks';
 import LineProvider from './LineProvider';
 import { useLineConfig } from './config';
+import TooltipMesh from '../shared/TooltipMesh';
 
 function LineVis(): JSX.Element {
   const props = useProps();
+  const { data } = props;
   const [showGrid, hasYLogScale] = useLineConfig(
     state => [state.showGrid, state.hasYLogScale],
     shallow
@@ -26,6 +29,14 @@ function LineVis(): JSX.Element {
       >
         {/* Provide context again - https://github.com/react-spring/react-three-fiber/issues/262 */}
         <LineProvider {...props}>
+          <TooltipMesh
+            formatIndex={([x]) => `x=${Math.floor(x)}`}
+            formatValue={([x]) =>
+              data[Math.floor(x)]
+                ? format('.3f')(data[Math.floor(x)])
+                : undefined
+            }
+          />
           <PanZoomMesh />
           <DataLine />
         </LineProvider>
--- src/h5web/visualizations/shared/TooltipMesh.tsx
+++ src/h5web/visualizations/shared/TooltipMesh.tsx
@@ -1,23 +1,19 @@
 import React, { ReactElement, useCallback } from 'react';
 import { Dom, PointerEvent, useThree } from 'react-three-fiber';
 import { TooltipWithBounds, useTooltip } from '@vx/tooltip';
-import { scaleLinear } from 'd3-scale';
-import { format } from 'd3-format';
+
 import styles from './TooltipMesh.module.css';
-import { Dims } from './models';
+import { useAbscissaScale, useOrdinateScale } from './utils';
 
 type Coords = [number, number];
 
 interface Props {
-  dims: Dims;
-  data: number[][];
+  formatIndex: (t: Coords) => string;
+  formatValue: (t: Coords) => string | undefined;
 }
 
 function TooltipMesh(props: Props): ReactElement {
-  const {
-    dims: [rows, cols],
-    data,
-  } = props;
+  const { formatIndex, formatValue } = props;
 
   const { camera, size } = useThree();
   const { width, height } = size;
@@ -32,12 +28,8 @@ function TooltipMesh(props: Props): ReactElement {
   } = useTooltip<Coords>();
 
   // Scales to compute data coordinates from unprojected mesh coordinates
-  const xCoordScale = scaleLinear()
-    .domain([-width / 2, width / 2])
-    .range([0, cols]);
-  const yCoordScale = scaleLinear()
-    .domain([-height / 2, height / 2])
-    .range([0, rows]);
+  const { abscissaScale } = useAbscissaScale();
+  const { ordinateScale } = useOrdinateScale();
 
   // Update tooltip when pointer moves
   // When panning, events are handled and stopped by texture mesh and do not reach this mesh (which is behind)
@@ -46,21 +38,23 @@ function TooltipMesh(props: Props): ReactElement {
       const { zoom } = camera;
       const projectedPoint = camera.worldToLocal(evt.unprojectedPoint.clone());
 
-      const xCoord = Math.floor(xCoordScale(evt.unprojectedPoint.x));
-      const yCoord = Math.floor(yCoordScale(evt.unprojectedPoint.y));
+      const abscissa = abscissaScale.invert(evt.unprojectedPoint.x);
+      const ordinate = ordinateScale.invert(evt.unprojectedPoint.y);
 
       showTooltip({
         tooltipLeft: projectedPoint.x * zoom + width / 2,
         tooltipTop: -projectedPoint.y * zoom + height / 2,
-        tooltipData: [xCoord, yCoord],
+        tooltipData: [abscissa, ordinate],
       });
     },
-    [yCoordScale, camera, height, xCoordScale, showTooltip, width]
+    [camera, abscissaScale, ordinateScale, showTooltip, width, height]
   );
 
   // Hide tooltip when pointer leaves mesh or user starts panning
   const onPointerOut = useCallback(hideTooltip, [hideTooltip]);
   const onPointerDown = useCallback(hideTooltip, [hideTooltip]);
+
+  const value = tooltipData && formatValue(tooltipData);
 
   return (
     <>
@@ -68,17 +62,15 @@ function TooltipMesh(props: Props): ReactElement {
         <planeBufferGeometry attach="geometry" args={[width, height]} />
       </mesh>
       <Dom style={{ width, height }}>
-        {tooltipOpen && tooltipData ? (
+        {tooltipOpen && tooltipData && value ? (
           <TooltipWithBounds
             key={Math.random()}
             className={styles.tooltip}
             top={tooltipTop}
             left={tooltipLeft}
           >
-            {`x=${tooltipData[0]}, y=${tooltipData[1]}`}
-            <span className={styles.tooltipValue}>
-              {format('.3')(data[tooltipData[1]][tooltipData[0]])}
-            </span>
+            {formatIndex(tooltipData)}
+            <span className={styles.tooltipValue}>{value}</span>
           </TooltipWithBounds>
         ) : (
           <></>
