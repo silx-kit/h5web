7bb9abc0610050352b6b40ca87bbf023a189f28c
Axel Bocciarelli <axelboc@gmail.com>
2020-05-18 15:12:46 +0200

    Sort and group colour maps

--- src/h5web/visualizations/heatmap/models.ts
+++ src/h5web/visualizations/heatmap/models.ts
@@ -5,3 +5,8 @@ export type Dims = [number, number];
 export type D3Interpolator = (t: number) => string;
 
 export type ColorMap = keyof typeof INTERPOLATORS;
+
+export type ColorMapOption = {
+  label: ColorMap;
+  value: ColorMap;
+};
