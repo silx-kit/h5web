2ce4b4405d8d142914267754d9a21718d1f7508b
Axel Bocciarelli <axelboc@gmail.com>
2020-06-08 09:28:13 +0200

    Turn domain slider into controlled component

--- src/h5web/visualizations/heatmap/DomainSlider.tsx
+++ src/h5web/visualizations/heatmap/DomainSlider.tsx
@@ -1,41 +1,32 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import ReactSlider from 'react-slider';
-import shallow from 'zustand/shallow';
 import { format } from 'd3-format';
 import { round, debounce } from 'lodash-es';
 import { FiRotateCcw } from 'react-icons/fi';
 import styles from './DomainSlider.module.css';
-import { useHeatmapConfig } from './config';
 import type { Domain } from '../shared/models';
 import { extendDomain } from '../shared/utils';
 
 const EXTEND_FACTOR = 0.2;
 const NB_DECIMALS = 1;
 
-function DomainSlider(): JSX.Element {
-  const [
-    dataDomain,
-    customDomain,
-    setCustomDomain,
-  ] = useHeatmapConfig((state) => [
-    state.dataDomain,
-    state.customDomain,
-    state.setCustomDomain,
-    shallow,
-  ]);
+interface Props {
+  dataDomain: Domain;
+  value?: Domain;
+  onChange: (value: Domain | undefined) => void;
+}
 
-  if (!dataDomain) {
-    return <></>;
-  }
+function DomainSlider(props: Props): ReactElement {
+  const { dataDomain, value, onChange } = props;
 
   const [extendedMin, extendedMax] = extendDomain(dataDomain, EXTEND_FACTOR);
   const step = Math.max((extendedMax - extendedMin) / 100, 10 ** -NB_DECIMALS);
 
-  const updateCustomDomain = debounce((values) => {
-    const roundedDomain = (values as number[]).map((value) =>
-      round(value, NB_DECIMALS)
+  const updateCustomDomain = debounce((bounds) => {
+    const roundedDomain = (bounds as number[]).map((val) =>
+      round(val, NB_DECIMALS)
     ) as Domain;
-    setCustomDomain(roundedDomain);
+    onChange(roundedDomain);
   }, 500);
 
   return (
@@ -43,20 +34,20 @@ function DomainSlider(): JSX.Element {
       <button
         className={styles.resetButton}
         type="button"
-        onClick={() => setCustomDomain(undefined)}
-        disabled={!customDomain}
+        onClick={() => onChange(undefined)}
+        disabled={!value}
       >
         <FiRotateCcw />
       </button>
       <ReactSlider
-        key={JSON.stringify(customDomain || dataDomain)}
+        key={JSON.stringify(value || dataDomain)}
         className={styles.slider}
         trackClassName={styles.sliderTrack}
         thumbClassName={styles.sliderThumb}
-        renderThumb={(props, state) => (
-          <div {...props}>{format(`.${NB_DECIMALS}f`)(state.valueNow)}</div>
+        renderThumb={(thumbProps, { valueNow }) => (
+          <div {...thumbProps}>{format(`.${NB_DECIMALS}f`)(valueNow)}</div>
         )}
-        defaultValue={[...(customDomain || dataDomain)]}
+        defaultValue={[...(value || dataDomain)]}
         onChange={updateCustomDomain}
         min={extendedMin}
         max={extendedMax}
