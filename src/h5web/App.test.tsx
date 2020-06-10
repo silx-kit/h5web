7720e9cf25fabbeecd7a48729809ef887bfa3e21
Loïc Huder <loic.huder@esrf.fr>
2020-06-10 12:27:54 +0200

    Added tests for mappings

--- src/h5web/App.test.tsx
+++ src/h5web/App.test.tsx
@@ -4,6 +4,8 @@ import {
   RenderResult,
   fireEvent,
   waitFor,
+  getByRole as getByRoleInElement,
+  getAllByRole as getAllByRoleInElement,
 } from '@testing-library/react';
 import App from './App';
 import MockProvider from './providers/mock/MockProvider';
@@ -198,4 +200,103 @@ test('inspect a dataset', async () => {
 
   const shapeRow = getByRole('row', { name: /Shape/ });
   expect(shapeRow).toBeVisible();
+});
+
+test('display mapping for X axis when visualizing 2D dataset as Line', async () => {
+  const {
+    getByRole,
+    getByLabelText,
+    queryByLabelText,
+    findByRole,
+  } = renderApp();
+
+  // Select the 2D dataset nD/twoD
+  fireEvent.click(await findByRole('treeitem', { name: 'nD' }));
+  fireEvent.click(getByRole('treeitem', { name: 'twoD' }));
+
+  // Select the line vis
+  fireEvent.click(await findByRole('tab', { name: 'Line' }));
+
+  // Ensure that the dimension mapper is only visible for X (and not for Y)
+  const xRadioGroup = getByLabelText('Dimension as x axis');
+  expect(xRadioGroup).toBeVisible();
+  const xDimsButtons = getAllByRoleInElement(xRadioGroup, 'radio');
+  expect(xDimsButtons).toHaveLength(2);
+  const yRadioGroup = queryByLabelText('Dimension as y axis');
+  expect(yRadioGroup).not.toBeInTheDocument();
+
+  // Ensure that the default mapping is [0, 'x']
+  expect(xDimsButtons[0]).not.toBeChecked();
+  expect(xDimsButtons[1]).toBeChecked();
+  const D0Slider = getByRole('slider');
+  expect(D0Slider).toHaveAttribute('aria-valueNow', '0');
+
+  // Ensure that the swap from [0, 'x'] to ['x', 0] works
+  fireEvent.click(xDimsButtons[0]);
+  expect(xDimsButtons[0]).toBeChecked();
+  expect(xDimsButtons[1]).not.toBeChecked();
+  const D1Slider = getByRole('slider');
+  expect(D1Slider).toHaveAttribute('aria-valueNow', '0');
+});
+
+test('display mappings for X and Y axes when visualizing 2D dataset as Heatmap', async () => {
+  const { getByRole, getByLabelText, findByRole } = renderApp();
+
+  // Select the 2D dataset nD/twoD
+  fireEvent.click(await findByRole('treeitem', { name: 'nD' }));
+  fireEvent.click(getByRole('treeitem', { name: 'twoD' }));
+
+  // Ensure that the heatmap vis is selected
+  const heatmapTab = await findByRole('tab', { name: 'Heatmap' });
+  expect(heatmapTab).toHaveAttribute('aria-selected', 'true');
+
+  // Ensure that the dimension mapper is visible for X and Y
+  const xRadioGroup = getByLabelText('Dimension as x axis');
+  expect(xRadioGroup).toBeVisible();
+  const yRadioGroup = getByLabelText('Dimension as y axis');
+  expect(yRadioGroup).toBeVisible();
+
+  // Ensure that the default mapping is ['y', 'x']
+  const xD1Button = getByRoleInElement(xRadioGroup, 'radio', { name: 'D1' });
+  expect(xD1Button).toBeChecked();
+  const yD0Button = getByRoleInElement(yRadioGroup, 'radio', { name: 'D0' });
+  expect(yD0Button).toBeChecked();
+
+  // Ensure that the swap from ['y', 'x'] to ['x', 'y'] works
+  const xD0Button = getByRoleInElement(xRadioGroup, 'radio', { name: 'D0' });
+  fireEvent.click(xD0Button);
+  expect(xD0Button).toBeChecked();
+  expect(xD1Button).not.toBeChecked();
+
+  const yD1Button = getByRoleInElement(yRadioGroup, 'radio', { name: 'D1' });
+  expect(yD0Button).not.toBeChecked();
+  expect(yD1Button).toBeChecked();
+});
+
+test('display one dimension slider and mappings for X and Y axes when visualizing 3D dataset as Matrix', async () => {
+  const { getByRole, getByLabelText, findByRole } = renderApp();
+
+  // Select the 3D dataset nD/threeD
+  fireEvent.click(await findByRole('treeitem', { name: 'nD' }));
+  fireEvent.click(getByRole('treeitem', { name: 'threeD' }));
+
+  // Select the matrix vis
+  fireEvent.click(await findByRole('tab', { name: 'Matrix' }));
+
+  // Ensure that the dimension mapper is visible for X and Y
+  const xRadioGroup = getByLabelText('Dimension as x axis');
+  expect(xRadioGroup).toBeVisible();
+  const xDimsButtons = getAllByRoleInElement(xRadioGroup, 'radio');
+  expect(xDimsButtons).toHaveLength(3);
+
+  const yRadioGroup = getByLabelText('Dimension as y axis');
+  expect(yRadioGroup).toBeVisible();
+  const yDimsButtons = getAllByRoleInElement(yRadioGroup, 'radio');
+  expect(yDimsButtons).toHaveLength(3);
+
+  // Ensure that the default mapping is [0, 'y', 'x']
+  expect(xDimsButtons[2]).toBeChecked();
+  expect(yDimsButtons[1]).toBeChecked();
+  const D0Slider = getByRole('slider');
+  expect(D0Slider).toHaveAttribute('aria-valueNow', '0');
 });
