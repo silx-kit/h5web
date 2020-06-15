d53dd355956286805c9b8eacc251327d23d1f18b
Axel Bocciarelli <axelboc@gmail.com>
2020-06-15 14:28:44 +0200

    Fix extraneous texture computations
    Fix #175

--- src/h5web/visualizations/heatmap/hooks.ts
+++ src/h5web/visualizations/heatmap/hooks.ts
@@ -43,6 +43,14 @@ export function useTextureData(
   const { proxy } = useComlink<TextureWorker>(() => new Worker(), []);
   const [state, mergeState] = useSetState<TextureDataState>({});
 
+  /*
+   * Dependencies that trigger a recomputation of the texture.
+   * > Note that `values` is purposely not included. When `values` changes, a first render
+   * > is triggered, during which `dataDomain` has not yet been recomputed. We must wait for
+   * > `domain` to be updated before recomputing the texture.
+   */
+  const deps = [colorMap, domain, hasLogScale, proxy, mergeState];
+
   useEffect(() => {
     if (!domain) {
       return;
@@ -65,7 +73,7 @@ export function useTextureData(
         ),
       });
     })();
-  }, [colorMap, domain, hasLogScale, proxy, mergeState, values]);
+  }, deps); // eslint-disable-line react-hooks/exhaustive-deps
 
   // Reset texture when dimensions change to avoid rendering glitch while computing new texture
   const dims = `${rows}x${cols}`;
