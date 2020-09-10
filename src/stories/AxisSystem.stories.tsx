15a8d0fad9906e790d1a9af67bc0f66514705fa3
Axel Bocciarelli <axelboc@gmail.com>
2020-09-10 12:06:45 +0200

    Make AxisSystem stories take all available space

--- src/stories/AxisSystem.stories.tsx
+++ src/stories/AxisSystem.stories.tsx
@@ -44,18 +44,26 @@ export const AspectRatio = Template.bind({});
 
 AspectRatio.args = {
   abscissaConfig: { indexDomain: [0, 10], showGrid: true },
-  ordinateConfig: { indexDomain: [0, 1], showGrid: true },
-  aspectRatio: 10,
+  ordinateConfig: { indexDomain: [0, 2], showGrid: true },
+  aspectRatio: 10 / 2,
 };
 
 export const NoGrid = Template.bind({});
 
 NoGrid.args = {
-  abscissaConfig: { indexDomain: [-5, 20] },
-  ordinateConfig: { dataDomain: [0, 1] },
+  abscissaConfig: { indexDomain: [-5, 20], showGrid: false },
+  ordinateConfig: { dataDomain: [0, 2], showGrid: false },
 };
 
 export default {
   title: 'VisCanvas/AxisSystem',
   component: VisCanvas,
+  parameters: { layout: 'fullscreen' },
+  decorators: [
+    (VisCanvasStory: Story) => (
+      <div style={{ display: 'flex', height: '100vh' }}>
+        <VisCanvasStory />
+      </div>
+    ),
+  ],
 };
