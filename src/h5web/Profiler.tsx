dea9e4121e71cda9ae79cea00b22419775f5b7a1
Axel Bocciarelli <axelboc@gmail.com>
2020-09-14 17:34:11 +0200

    Allow profiling visualizations

---
+++ src/h5web/Profiler.tsx
@@ -0,0 +1,35 @@
+import React, { Profiler as ReactProfiler, ReactElement } from 'react';
+
+interface Props {
+  id: string;
+  children: ReactElement;
+}
+
+function Profiler(props: Props): ReactElement {
+  const { id, children } = props;
+
+  if (!process.env.REACT_APP_PROFILING_ENABLED) {
+    return children;
+  }
+
+  return (
+    <ReactProfiler
+      id={id}
+      onRender={(_, phase, actualDuration, baseDuration) => {
+        // eslint-disable-next-line no-console
+        console.debug(
+          id,
+          phase,
+          '- duration =',
+          actualDuration,
+          '- worst-case =',
+          baseDuration
+        );
+      }}
+    >
+      {children}
+    </ReactProfiler>
+  );
+}
+
+export default Profiler;
