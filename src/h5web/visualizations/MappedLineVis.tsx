59fd74107427217923be90908aeb0585b4a7a8ae
Axel Bocciarelli <axelboc@gmail.com>
2020-09-17 14:11:44 +0200

    Turn MappedVis into a hook

--- src/h5web/visualizations/MappedLineVis.tsx
+++ src/h5web/visualizations/MappedLineVis.tsx
@@ -1,9 +1,9 @@
 import React, { ReactElement } from 'react';
-import MappedVis from './shared/MappedVis';
 import type { HDF5Dataset, HDF5Value } from '../providers/models';
 import type { DimensionMapping } from '../dataset-visualizer/models';
 import LineVis from './line/LineVis';
 import { assertArray } from './shared/utils';
+import { useMappedArray } from './shared/hooks';
 
 interface Props {
   value: HDF5Value;
@@ -15,14 +15,8 @@ function MappedLineVis(props: Props): ReactElement {
   const { value, dataset, mapperState } = props;
   assertArray<number>(value);
 
-  return (
-    <MappedVis
-      component={LineVis}
-      dataset={dataset}
-      value={value}
-      mapperState={mapperState}
-    />
-  );
+  const dataArray = useMappedArray(dataset, value, mapperState);
+  return <LineVis dataArray={dataArray} />;
 }
 
 export default MappedLineVis;
--- src/h5web/visualizations/MappedMatrixVis.tsx
+++ src/h5web/visualizations/MappedMatrixVis.tsx
@@ -1,9 +1,9 @@
 import React, { ReactElement } from 'react';
-import MappedVis from './shared/MappedVis';
 import type { HDF5Dataset, HDF5Value } from '../providers/models';
 import type { DimensionMapping } from '../dataset-visualizer/models';
 import MatrixVis from './matrix/MatrixVis';
 import { assertArray } from './shared/utils';
+import { useMappedArray } from './shared/hooks';
 
 interface Props {
   value: HDF5Value;
@@ -15,14 +15,8 @@ function MappedMatrixVis(props: Props): ReactElement {
   const { value, dataset, mapperState } = props;
   assertArray<number | string>(value);
 
-  return (
-    <MappedVis
-      component={MatrixVis}
-      dataset={dataset}
-      value={value}
-      mapperState={mapperState}
-    />
-  );
+  const dataArray = useMappedArray(dataset, value, mapperState);
+  return <MatrixVis dataArray={dataArray} />;
 }
 
 export default MappedMatrixVis;
