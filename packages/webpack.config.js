55618b5990e74da1afc1325f4273309bb4ab4533
Axel Bocciarelli <axelboc@gmail.com>
2020-09-25 14:09:44 +0200

    Support publishing multiple packages (app vs. lib)

---
+++ packages/webpack.config.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const packagesPath = path.resolve(__dirname, '../src/packages');
+const packages = fs.readdirSync(packagesPath);
+
+function getConfig(pkg) {
+  const name = path.basename(pkg, '.ts');
+  const outDir = path.resolve(__dirname, `dist-${name}`);
+
+  return {
+    mode: 'production',
+    devtool: 'source-map',
+    context: path.resolve(__dirname),
+    entry: { [name]: path.join(packagesPath, pkg) },
+    output: {
+      path: outDir,
+      filename: '[name].js',
+      libraryTarget: 'commonjs2',
+    },
+    externals: {
+      react: 'react',
+      'react-dom': 'react-dom',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.wasm', '.mjs', '.js', '.json'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: path.resolve(__dirname, 'tsconfig.json'),
+                onlyCompileBundledFiles: true,
+                compilerOptions: { outDir },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [{ loader: 'url-loader' }],
+        },
+        {
+          test: /\.css$/,
+          include: /\.module\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: {
+                  localIdentName: '[name]__[local]___[hash:base64:5]',
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          exclude: /\.module\.css$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            { loader: 'css-loader' },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: `package-${name}.json`,
+            to: `package.json`,
+          },
+        ],
+      }),
+    ],
+  };
+}
+
+module.exports = packages.map(getConfig);
---
+++ src/packages/lib.ts
@@ -0,0 +1,3 @@
+export { default as HeatmapVis } from '../h5web/visualizations/heatmap/HeatmapVis';
+export { default as mockData } from '../h5web/providers/mock/data.json';
+export { findDomain } from '../h5web/visualizations/shared/utils';
