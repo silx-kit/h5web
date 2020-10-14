ca6d8445685fb72ce21656f355e506f00b53143b
Axel Bocciarelli <axelboc@gmail.com>
2020-10-14 12:16:25 +0200

    Fail tests on console errors and warnings

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,4 @@
-// https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'; // https://github.com/testing-library/jest-dom
 
 // Define properties not supported by JSDOM to avoid Re-flex warnings
 // https://github.com/leefsmp/Re-Flex/issues/27
@@ -9,4 +8,25 @@ import '@testing-library/jest-dom';
     configurable: true,
     value: 500,
   });
+});
+
+// Fail if error or warning has been logged to the console (notably by React or React Testing Library)
+// Inpsired by https://github.com/facebook/jest/issues/6121#issuecomment-493529970
+let consoleHasErrorOrWarning = false;
+const { error, warn } = console;
+
+global.console.error = (...args) => {
+  consoleHasErrorOrWarning = true;
+  error(...args);
+};
+global.console.warn = (...args) => {
+  consoleHasErrorOrWarning = true;
+  warn(...args);
+};
+
+afterEach(() => {
+  if (consoleHasErrorOrWarning) {
+    consoleHasErrorOrWarning = false;
+    throw new Error('Console has error or warning');
+  }
 });
