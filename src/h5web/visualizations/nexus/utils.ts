452d073fb58adde41aa7de7d9362d61e8ed4ce0e
Axel Bocciarelli <axelboc@gmail.com>
2020-12-03 12:39:51 +0100

    Refactor `supportsEntity` functions with nested HDF5 structure

--- src/h5web/visualizations/nexus/utils.ts
+++ src/h5web/visualizations/nexus/utils.ts
@@ -6,14 +6,22 @@ import type {
   HDF5Entity,
   HDF5NumericType,
   HDF5SimpleShape,
+  MyHDF5Group,
+  MyHDF5Entity,
+  MyHDF5Dataset,
 } from '../../providers/models';
 import {
   assertGroup,
+  assertMyGroup,
   assertDataset,
+  assertMyDataset,
   assertNumericType,
+  assertMyNumericType,
   assertSimpleShape,
+  assertMySimpleShape,
   getLinkedEntity,
   isGroup,
+  isMyGroup,
 } from '../../providers/utils';
 import {
   NxAttribute,
@@ -29,10 +37,17 @@ import {
 } from '../shared/utils';
 
 export function getAttributeValue(
-  entity: HDF5Dataset | HDF5Group,
+  entity: HDF5Dataset | HDF5Group | MyHDF5Entity,
   attributeName: NxAttribute
 ): HDF5Value | undefined {
   return entity.attributes?.find((attr) => attr.name === attributeName)?.value;
+}
+
+function getChildEntity(
+  group: MyHDF5Group,
+  entityName: string
+): MyHDF5Entity | undefined {
+  return group.children.find((child) => child.name === entityName);
 }
 
 export function findNxDataGroup(
@@ -68,7 +83,43 @@ export function findNxDataGroup(
   return findNxDataGroup(defaultEntity, metadata);
 }
 
-export function findSignalName(group: HDF5Group): string {
+export function findMyNxDataGroup(group: MyHDF5Group): MyHDF5Group | undefined {
+  if (getAttributeValue(group, 'NX_class') === 'NXdata') {
+    return group; // `NXdata` group found
+  }
+
+  const defaultPath = getAttributeValue(group, 'default');
+  if (defaultPath === undefined) {
+    return undefined; // group has no `default` attribute
+  }
+
+  assertStr(defaultPath, `Expected 'default' attribute to be a string`);
+
+  const isAbsolutePath = defaultPath.startsWith('/');
+  const pathSegments = defaultPath.slice(isAbsolutePath ? 1 : 0).split('/');
+
+  const defaultEntity = pathSegments.reduce<MyHDF5Entity | undefined>(
+    (parentEntity, currSegment) => {
+      return parentEntity && isMyGroup(parentEntity)
+        ? getChildEntity(parentEntity, currSegment)
+        : undefined;
+    },
+    isAbsolutePath ? group.parents[0] : group
+  );
+
+  assertDefined(defaultEntity, `Expected entity at path "${defaultPath}"`);
+  assertMyGroup(defaultEntity, `Expected group at path "${defaultPath}"`);
+
+  const nxDataGroup = findMyNxDataGroup(defaultEntity);
+  assertDefined(
+    nxDataGroup,
+    `Expected NXdata group or group with 'default' attribute`
+  );
+
+  return nxDataGroup;
+}
+
+export function findSignalName(group: HDF5Group | MyHDF5Group): string {
   const signal = getAttributeValue(group, 'signal');
 
   assertDefined(signal, "Expected 'signal' attribute");
@@ -88,6 +139,20 @@ export function findSignalDataset(
   assertDataset(dataset, `Expected "${signalName}" signal to be a dataset`);
   assertNumericType(dataset);
   assertSimpleShape(dataset);
+
+  return dataset;
+}
+
+export function findMySignalDataset(
+  group: MyHDF5Group,
+  signalName: string
+): MyHDF5Dataset<HDF5SimpleShape, HDF5NumericType> {
+  const dataset = getChildEntity(group, signalName);
+
+  assertDefined(dataset, `Expected "${signalName}" signal entity to exist`);
+  assertMyDataset(dataset, `Expected "${signalName}" signal to be a dataset`);
+  assertMyNumericType(dataset);
+  assertMySimpleShape(dataset);
 
   return dataset;
 }
