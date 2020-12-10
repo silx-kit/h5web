ec8d21f0dc52c98491acad29552211dda0f528f2
Loic Huder <loic.huder@esrf.fr>
2020-12-10 10:31:33 +0100

    Error toggle in NxSpectrum config is now persisted

--- src/h5web/visualizations/nexus/config.ts
+++ src/h5web/visualizations/nexus/config.ts
@@ -1,17 +1,23 @@
-import create from 'zustand';
 import { combine } from 'zustand/middleware';
+import { createPersistableState, StorageConfig } from '../../storage-utils';
 
 interface NxSpectrumConfig {
   showErrors: boolean;
   areErrorsDisabled: boolean;
 }
 
+const STORAGE_CONFIG: StorageConfig = {
+  storageId: 'h5web:nxSpectrum',
+  itemsToPersist: ['showErrors'],
+};
+
 const INITIAL_STATE: NxSpectrumConfig = {
   showErrors: true,
   areErrorsDisabled: false,
 };
 
-export const useNxSpectrumConfig = create(
+export const useNxSpectrumConfig = createPersistableState(
+  STORAGE_CONFIG,
   combine({ ...INITIAL_STATE }, (set) => ({
     toggleErrors: () => set((state) => ({ showErrors: !state.showErrors })),
     disableErrors: (areErrorsDisabled: boolean) => set({ areErrorsDisabled }),
