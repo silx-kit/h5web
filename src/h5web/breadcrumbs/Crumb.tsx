f8bf0f37b306430490a6ff719a16b846d1b80034
Loic Huder <loic.huder@esrf.fr>
2021-02-03 16:06:29 +0100

    Made crumbs clickable to navigate to previous groups

---
+++ src/h5web/breadcrumbs/Crumb.tsx
@@ -0,0 +1,23 @@
+import type { ReactElement } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import styles from './BreadcrumbsBar.module.css';
+
+interface Props {
+  name: string;
+  onClick: () => void;
+}
+
+function Crumb(props: Props): ReactElement {
+  const { name, onClick } = props;
+
+  return (
+    <>
+      <button className={styles.crumbButton} type="button" onClick={onClick}>
+        <span className={styles.crumb}>{name}</span>
+      </button>
+      <FiChevronRight className={styles.separator} title="/" />
+    </>
+  );
+}
+
+export default Crumb;
