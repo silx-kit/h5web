4e5e28a181c2ff87b2fea5c5a69ec1d6c31efe2e
Axel Bocciarelli <axelboc@gmail.com>
2021-03-01 16:53:54 +0100

    Use scale to interface with domain slider

--- src/h5web/toolbar/controls/DomainSlider/hooks.ts
+++ src/h5web/toolbar/controls/DomainSlider/hooks.ts
@@ -1,18 +1,4 @@
-import { useEffect, useState } from 'react';
-import type { Domain } from '../../../../packages/lib';
-import type { CustomDomain } from '../../../vis-packs/core/models';
-import { getSliderDomain } from './utils';
+import { createMemo } from 'react-use';
+import { getVisDomain } from './utils';
 
-export function useAppliedDomain(
-  dataDomain: Domain,
-  customDomain: CustomDomain
-) {
-  const state = useState(getSliderDomain(dataDomain, customDomain));
-  const [, setAppliedDomain] = state;
-
-  useEffect(() => {
-    setAppliedDomain(getSliderDomain(dataDomain, customDomain));
-  }, [customDomain, dataDomain, setAppliedDomain]);
-
-  return state;
-}
+export const useVisDomain = createMemo(getVisDomain);
