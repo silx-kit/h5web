b3f5d7e0ca3eb683952c37e0e06e166136738a8d
Axel Bocciarelli <axelboc@gmail.com>
2021-03-02 16:29:34 +0100

    Update only domain bounds that change when interacting with slider

--- src/h5web/toolbar/controls/DomainSlider/ScaledSlider.tsx
+++ src/h5web/toolbar/controls/DomainSlider/ScaledSlider.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement, Ref } from 'react';
 import { FiSkipBack, FiSkipForward } from 'react-icons/fi';
 import ReactSlider from 'react-slider';
+import { useBoolean } from 'react-use';
 import { Domain, extendDomain, ScaleType } from '../../../../packages/lib';
 import { createAxisScale } from '../../../vis-packs/core/utils';
 import styles from './DomainSlider.module.css';
@@ -15,15 +16,22 @@ interface Props {
   disabled?: boolean;
   isAutoMin: boolean;
   isAutoMax: boolean;
-  onChange: (domain: Domain) => void;
-  onAfterChange: (domain: Domain) => void;
+  onChange: (
+    newValue: Domain,
+    hasMinChanged: boolean,
+    hasMaxChanged: boolean
+  ) => void;
+  onAfterChange: (hasMinChanged: boolean, hasMaxChanged: boolean) => void;
 }
 
 function ScaledSlider(props: Props): ReactElement {
   const { value, dataDomain, disabled, isAutoMin, isAutoMax } = props;
-  const { onChange, onAfterChange } = props;
+  const { onChange, onAfterChange: onDone } = props;
 
   const sliderExtent = extendDomain(dataDomain, EXTEND_FACTOR);
+
+  const [hasMinChanged, setMinChanged] = useBoolean(false);
+  const [hasMaxChanged, setMaxChanged] = useBoolean(false);
 
   const scale = createAxisScale({
     type: ScaleType.Linear,
@@ -31,6 +39,29 @@ function ScaledSlider(props: Props): ReactElement {
     range: SLIDER_RANGE,
     round: true,
   });
+
+  const scaledValue = value.map(scale) as Domain;
+
+  function handleChange(newScaledValue: Domain) {
+    const [newScaledMin, newScaledMax] = newScaledValue;
+    const hasMinChanged = newScaledMin !== scaledValue[0];
+    const hasMaxChanged = newScaledMax !== scaledValue[1];
+
+    const newValue: Domain = [
+      hasMinChanged ? scale.invert(newScaledMin) : value[0],
+      hasMaxChanged ? scale.invert(newScaledMax) : value[1],
+    ];
+
+    onChange(newValue, hasMinChanged, hasMaxChanged);
+    setMinChanged(hasMinChanged);
+    setMaxChanged(hasMaxChanged);
+  }
+
+  function handleAfterChange() {
+    onDone(hasMinChanged, hasMaxChanged);
+    setMinChanged(false);
+    setMaxChanged(false);
+  }
 
   return (
     <ReactSlider
@@ -41,16 +72,10 @@ function ScaledSlider(props: Props): ReactElement {
       pearling
       min={SLIDER_RANGE[0]}
       max={SLIDER_RANGE[1]}
-      value={value.map(scale) as Domain}
+      value={scaledValue}
       disabled={disabled}
-      onChange={(bounds) => {
-        onChange((bounds as Domain).map((val) => scale.invert(val)) as Domain);
-      }}
-      onAfterChange={(bounds) => {
-        onAfterChange(
-          (bounds as Domain).map((val) => scale.invert(val)) as Domain
-        );
-      }}
+      onChange={(bounds) => handleChange(bounds as Domain)}
+      onAfterChange={handleAfterChange}
       renderThumb={({ ref, ...thumbProps }, { index }) => (
         <Thumb
           ref={ref as Ref<HTMLDivElement>}
