143a1fb9c12c012c32048b0e8cfa97765aeb0867
Axel Bocciarelli <axelboc@gmail.com>
2021-03-18 17:23:54 +0100

    Allow any ReactNode as children of Toolbar

--- src/h5web/toolbar/Toolbar.tsx
+++ src/h5web/toolbar/Toolbar.tsx
@@ -1,48 +1,38 @@
-import { ReactElement, Children, Fragment } from 'react';
-import { useMeasure, useMap } from 'react-use';
+import { ReactElement, Children, ReactNode } from 'react';
+import { useMeasure, useList } from 'react-use';
 import styles from './Toolbar.module.css';
 import Separator from './Separator';
 import OverflowMenu from './OverflowMenu';
 import MeasuredControl from './MeasuredControl';
 
 interface Props {
-  // Toolbar controls must be direct children of `Toolbar` (no fragment)
-  children?: (ReactElement | undefined)[] | ReactElement;
+  children?: ReactNode[];
 }
 
 function Toolbar(props: Props): ReactElement {
   const { children } = props;
   const allChildren = Children.toArray(children) as ReactElement[];
 
-  if (allChildren.filter((child) => child.type === Fragment).length > 0) {
-    throw new Error('Fragment not allowed as child of Toolbar');
-  }
-
   const [containerRef, { width: availableWidth }] = useMeasure();
-  const [childrenWidths, { set: setChildWidth }] = useMap<
-    Record<string, number>
-  >();
+  const [childrenWidths, { updateAt: setChildWidth }] = useList<number>();
 
-  // Filter out children that haven't been measured or with width of `0`
-  const measuredChildren = allChildren.filter(
-    (child) => !!childrenWidths[child.key as string]
+  // Group visible and hidden children based on their accumulated width
+  const [visibleChildren, hiddenChildren, allMeasured] = allChildren.reduce(
+    ([accVisible, accHidden, accAllMeasured, accWidth], child, index) => {
+      const width = childrenWidths[index] ?? 0;
+      const isMeasured = width > 0;
+      const isOverflowing = accWidth + width > availableWidth;
+
+      return [
+        isMeasured && !isOverflowing ? [...accVisible, child] : accVisible,
+        isMeasured && isOverflowing ? [...accHidden, child] : accHidden,
+        accAllMeasured && isMeasured,
+        accWidth + width,
+      ];
+    },
+    [[] as ReactElement[], [] as ReactElement[], true, 0]
   );
 
-  // Measure cumulative widths to find index of first overflowing child
-  const cumulativeWidths = measuredChildren.reduce<number[]>((acc, child) => {
-    const width = childrenWidths[child.key as string];
-    return [...acc, (acc[acc.length - 1] ?? 0) + width];
-  }, []);
-
-  const firstOverflowIndex = [...cumulativeWidths, Infinity].findIndex(
-    (width) => width > availableWidth
-  );
-
-  // Group visible and hidden children
-  const visibleChildren = measuredChildren.slice(0, firstOverflowIndex);
-  const hiddenChildren = measuredChildren.slice(firstOverflowIndex);
-
-  const allMeasured = measuredChildren.length === allChildren.length;
   const isSeparatorLast =
     visibleChildren[visibleChildren.length - 1]?.type === Separator;
 
@@ -62,7 +52,9 @@ function Toolbar(props: Props): ReactElement {
           {allOrVisibleChildren.map((child) => (
             <MeasuredControl
               key={`measure-${child.key || ''}`}
-              onMeasure={(width) => setChildWidth(child.key as string, width)}
+              onMeasure={(width) => {
+                setChildWidth(allChildren.indexOf(child), width);
+              }}
             >
               {child}
             </MeasuredControl>
