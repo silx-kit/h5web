f0aaa01f862c3fe4f6a50fb77b894f976c078270
Axel Bocciarelli <axelboc@gmail.com>
2021-04-13 15:59:05 +0200

    Upgrade dependencies and linting config

--- src/h5web/providers/utils.ts
+++ src/h5web/providers/utils.ts
@@ -1,7 +1,10 @@
 import { pickBy } from 'lodash-es';
 import { createFetchStore } from 'react-suspense-fetch';
 
-export interface ObjectKeyStore<R, I extends {}> {
+export interface ObjectKeyStore<
+  I extends Record<string, unknown>,
+  R = unknown
+> {
   get: (obj: I) => R;
   prefetch: (obj: I) => void;
   evict: (obj: I) => void;
@@ -15,11 +18,12 @@ export interface ObjectKeyStore<R, I extends {}> {
  * Interaction with the underlying `react-suspense-fetch` store is done by
  * JSON-stringifying/parsing the input object.
  */
-export function createObjectKeyStore<R, I extends {}>(
-  fetchFunc: (obj: I) => Promise<R>
-): ObjectKeyStore<R, I> {
+export function createObjectKeyStore<
+  I extends Record<string, unknown>,
+  R = unknown
+>(fetchFunc: (params: I) => Promise<R>): ObjectKeyStore<I, R> {
   const store = createFetchStore((key: string) => {
-    return fetchFunc(JSON.parse(key));
+    return fetchFunc(JSON.parse(key) as I);
   });
 
   function wrapMethod<T>(method: (key: string) => T) {
