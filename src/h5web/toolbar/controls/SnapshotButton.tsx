81ffe82d269cca94e99d4b8444a8e9af8b679181
Axel Bocciarelli <axelboc@gmail.com>
2021-07-02 13:16:23 +0200

    Abstract `DownloadBtn` out of `SnapshotBtn`

--- src/h5web/toolbar/controls/SnapshotButton.tsx
+++ src/h5web/toolbar/controls/SnapshotButton.tsx
@@ -1,33 +1,18 @@
 import { MdCameraAlt } from 'react-icons/md';
-import styles from './SnapshotButton.module.css';
+import DownloadBtn from './DownloadBtn';
 
 function SnapshotButton() {
   return (
-    <a
-      className={styles.link}
-      href="/"
-      target="_blank"
-      aria-label="Snapshot"
-      onClick={(evt) => {
+    <DownloadBtn
+      label="Snapshot"
+      icon={MdCameraAlt}
+      filename="snapshot"
+      getDownloadUrl={() => {
+        // Create data URL from canvas (if rendered)
         const canvas = document.querySelector('canvas');
-
-        // Create data URL from canvas
-        const screnshotUrl = canvas?.toDataURL();
-
-        if (screnshotUrl) {
-          // Let link open screenshot URL in new tab/window
-          evt.currentTarget.setAttribute('href', screnshotUrl);
-        } else {
-          // Don't follow link if canvas hasn't been rendered yet
-          evt.preventDefault();
-        }
+        return canvas?.toDataURL() || false;
       }}
-    >
-      <span className={styles.btnLike}>
-        <MdCameraAlt className={styles.icon} />
-        <span className={styles.label}>Snapshot</span>
-      </span>
-    </a>
+    />
   );
 }
 
