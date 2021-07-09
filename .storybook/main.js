2c3e809b0944908fa141eb1d68adab9fe4cc66ec
Axel Bocciarelli <axelboc@gmail.com>
2021-07-09 15:44:20 +0200

    Upgrade Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  typescript: {
-    // Temp fix of https://github.com/styleguidist/react-docgen-typescript/issues/356
-    // (https://github.com/styleguidist/react-docgen-typescript/issues/356#issuecomment-857887751)
-    reactDocgen: 'react-docgen',
-  },
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
