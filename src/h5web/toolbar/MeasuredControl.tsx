2172dc33a82b81fefe2d36d7ed4e1228de31b8b2
Axel Bocciarelli <axelboc@gmail.com>
2021-08-30 12:19:30 +0200

    Prevent entire toolbar from flickering when changing scale

--- src/h5web/toolbar/MeasuredControl.tsx
+++ src/h5web/toolbar/MeasuredControl.tsx
@@ -4,23 +4,29 @@ import styles from './Toolbar.module.css';
 
 interface Props {
   children: ReactElement;
+  knownWidth: number | undefined;
   onMeasure: (width: number) => void;
 }
 
 function MeasuredControl(props: Props) {
-  const { children: child, onMeasure } = props;
+  const { children: child, knownWidth, onMeasure } = props;
 
   return (
     <Measure
       onResize={({ entry }: { entry?: DOMRect }) => {
-        if (entry) {
+        if (entry && entry.width !== knownWidth) {
           onMeasure(entry.width);
         }
       }}
     >
       {({ measureRef }) => (
-        <div ref={measureRef} className={styles.control}>
-          {child}
+        <div
+          className={styles.controlWrapper}
+          data-measured={knownWidth !== undefined} // hide control until measured for the first time
+        >
+          <div ref={measureRef} className={styles.control}>
+            {child}
+          </div>
         </div>
       )}
     </Measure>
