a9217f3aa05f5e28f1f238c4279f7ed93e5d886d
Axel Bocciarelli <axelboc@gmail.com>
2021-10-01 11:31:48 +0200

    Fix styles in demo in dev

--- apps/demo/src/index.tsx
+++ apps/demo/src/index.tsx
@@ -4,14 +4,14 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
 import 'normalize.css';
+import './styles/index.css';
 
 if (process.env.REACT_APP_DIST === 'true') {
   require('@h5web/lib/dist/style.css');
   require('@h5web/app/dist/style.css');
 }
 
-require('./styles/index.css'); // `require` syntax to maintain correct order
-
+import '@h5web/lib'; // ensures that the lib's styles come first in development
 import DemoApp from './DemoApp';
 
 ReactDOM.render(
