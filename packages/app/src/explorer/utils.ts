caf1601b4b124e9f21f5a024b6dc71607aa13afc
Axel Bocciarelli <axelboc@gmail.com>
2021-11-16 09:15:18 +0100

    Fetch attribute values on demand for NX badges in explorer

--- packages/app/src/explorer/utils.ts
+++ packages/app/src/explorer/utils.ts
@@ -1,11 +1,15 @@
-import type { Entity } from '@h5web/shared';
-import { assertStr, isGroup } from '@h5web/shared';
+import type { AttributeValues, Entity } from '@h5web/shared';
+import { isGroup, assertStr } from '@h5web/shared';
+import type { FetchStore } from 'react-suspense-fetch';
 
-import { getAttributeValue, hasAttribute } from '../utils';
+import { hasAttribute } from '../utils';
 
 const SUPPORTED_NX_CLASSES = new Set(['NXdata', 'NXentry', 'NXprocess']);
 
-export function isNxGroup(entity: Entity): boolean {
+export function needsNxBadge(
+  entity: Entity,
+  attrValuesStore: FetchStore<AttributeValues, Entity>
+): boolean {
   if (!isGroup(entity)) {
     return false;
   }
@@ -15,7 +19,9 @@ export function isNxGroup(entity: Entity): boolean {
   }
 
   if (hasAttribute(entity, 'NX_class')) {
-    const nxClass = getAttributeValue(entity, 'NX_class');
+    const attrValues = attrValuesStore.get(entity);
+    const nxClass = attrValues?.NX_class;
+
     assertStr(nxClass);
     return SUPPORTED_NX_CLASSES.has(nxClass);
   }
