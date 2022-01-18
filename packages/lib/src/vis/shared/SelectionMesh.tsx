c9d1d4ed295dd07a0c4b02b8ec763204e2abd672
Loic Huder <loic.huder@esrf.fr>
2022-01-18 11:19:38 +0100

    Fix selections moving with respect to data on resize

--- packages/lib/src/vis/shared/SelectionMesh.tsx
+++ packages/lib/src/vis/shared/SelectionMesh.tsx
@@ -1,8 +1,10 @@
 import type { ThreeEvent } from '@react-three/fiber';
 import type { ReactElement } from 'react';
 import { useCallback, useState } from 'react';
+import type { Vector3 } from 'three';
 import { Vector2 } from 'three';
 
+import { useAxisSystemContext } from './AxisSystemContext';
 import VisMesh from './VisMesh';
 
 interface Props {
@@ -14,48 +16,73 @@ interface Props {
 
 function SelectionMesh(props: Props) {
   const { selectionComponent: Selection } = props;
+  const { abscissaScale, ordinateScale } = useAxisSystemContext();
+
+  const worldToData = useCallback(
+    (vec: Vector3) =>
+      new Vector2(abscissaScale.invert(vec.x), ordinateScale.invert(vec.y)),
+    [abscissaScale, ordinateScale]
+  );
+
+  const dataToWorld = useCallback(
+    (vec: Vector2) => new Vector2(abscissaScale(vec.x), ordinateScale(vec.y)),
+    [abscissaScale, ordinateScale]
+  );
 
   const [startPoint, setStartPoint] = useState<Vector2>();
   const [endPoint, setEndPoint] = useState<Vector2>();
   const [isDragging, setDrag] = useState(false);
 
-  const onPointerDown = useCallback((evt: ThreeEvent<PointerEvent>) => {
-    const { sourceEvent, unprojectedPoint } = evt;
-    const { target, pointerId } = sourceEvent;
-    (target as Element).setPointerCapture(pointerId);
-    setStartPoint(new Vector2(unprojectedPoint.x, unprojectedPoint.y));
-    setEndPoint(new Vector2(unprojectedPoint.x, unprojectedPoint.y));
-    setDrag(true);
-  }, []);
+  const onPointerDown = useCallback(
+    (evt: ThreeEvent<PointerEvent>) => {
+      const { sourceEvent, unprojectedPoint } = evt;
+      const { target, pointerId } = sourceEvent;
+      (target as Element).setPointerCapture(pointerId);
+
+      const point = worldToData(unprojectedPoint);
+
+      setStartPoint(point);
+      setEndPoint(point);
+      setDrag(true);
+    },
+    [worldToData]
+  );
 
   const onPointerMove = useCallback(
     (evt: ThreeEvent<PointerEvent>) => {
       if (!isDragging) {
         return;
       }
-      setEndPoint(new Vector2(evt.unprojectedPoint.x, evt.unprojectedPoint.y));
+      const point = worldToData(evt.unprojectedPoint);
+
+      setEndPoint(point);
     },
-    [isDragging]
+    [worldToData, isDragging]
   );
 
-  const onPointerUp = useCallback((evt: ThreeEvent<PointerEvent>) => {
-    const { sourceEvent, unprojectedPoint } = evt;
-    const { target, pointerId } = sourceEvent;
+  const onPointerUp = useCallback(
+    (evt: ThreeEvent<PointerEvent>) => {
+      const { sourceEvent, unprojectedPoint } = evt;
+      const { target, pointerId } = sourceEvent;
+      const point = worldToData(unprojectedPoint);
 
-    (target as Element).releasePointerCapture(pointerId);
-    setEndPoint(new Vector2(unprojectedPoint.x, unprojectedPoint.y));
-    setDrag(false);
-  }, []);
+      (target as Element).releasePointerCapture(pointerId);
+      setEndPoint(point);
+      setDrag(false);
+    },
+    [worldToData]
+  );
 
   return (
-    <>
-      <VisMesh {...{ onPointerMove, onPointerUp, onPointerDown }}>
-        <meshBasicMaterial opacity={0} transparent />
-      </VisMesh>
+    <VisMesh {...{ onPointerMove, onPointerUp, onPointerDown }}>
+      <meshBasicMaterial opacity={0} transparent />
       {startPoint && endPoint && (
-        <Selection startPoint={startPoint} endPoint={endPoint} />
+        <Selection
+          startPoint={dataToWorld(startPoint)}
+          endPoint={dataToWorld(endPoint)}
+        />
       )}
-    </>
+    </VisMesh>
   );
 }
 
