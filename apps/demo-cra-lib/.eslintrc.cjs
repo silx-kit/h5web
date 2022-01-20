71058c84e1be1cc1543be85c60fee12e5e62532e
Axel Bocciarelli <axelboc@gmail.com>
2022-01-20 15:32:13 +0100

    Remove `demo-cra-lib` project from repo (replaced with CodeSandbox)

--- apps/demo-cra-lib/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-const { createConfig } = require('eslint-config-galex/src/createConfig');
-
-const { rules, overrides } = require('../../eslint.shared');
-
-module.exports = createConfig({
-  cwd: __dirname,
-  rules,
-  overrides,
-});
