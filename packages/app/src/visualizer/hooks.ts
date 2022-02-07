086b87f15fc41c0c7ec9c338a31700b2ca250a86
Loic Huder <loic.huder@esrf.fr>
2022-02-07 15:10:43 +0100

    Restore last used vis when coming back to vis panel

--- packages/app/src/visualizer/hooks.ts
+++ packages/app/src/visualizer/hooks.ts
@@ -1,16 +1,26 @@
-import { useState } from 'react';
+import { useLocalStorageValue } from '@react-hookz/web';
 
 import type { VisDef } from '../vis-packs/models';
 
-export function useActiveVis<T extends VisDef>(supportedVis: T[]) {
+export function useActiveVis<T extends VisDef>(
+  supportedVis: T[]
+): [T, (v: T) => void] {
   const lastVis = supportedVis[supportedVis.length - 1];
 
-  const state = useState<T>(lastVis);
-  const [activeVis, setActiveVis] = state;
+  const [storedVisName, setStoredVisName] = useLocalStorageValue(
+    'h5web:visName',
+    lastVis.name,
+    {
+      handleStorageEvent: true,
+      storeDefaultValue: true,
+    }
+  );
 
-  if (!supportedVis.includes(activeVis)) {
-    setActiveVis(lastVis);
+  const activeVis =
+    supportedVis.find((v) => v.name === storedVisName) || lastVis;
+  if (activeVis.name !== storedVisName) {
+    setStoredVisName(lastVis.name);
   }
 
-  return state;
+  return [activeVis, (v: T) => setStoredVisName(v.name)];
 }
