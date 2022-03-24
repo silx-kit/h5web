11c3f9d9a71eac8f3b2a6f284ec4903c51468ae9
Brian Ben Maranville <brian.maranville@nist.gov>
2022-03-24 16:11:56 -0400

    need es2020 targets to support bigint

--- apps/demo/vite.config.js
+++ apps/demo/vite.config.js
@@ -1,10 +1,19 @@
 import react from '@vitejs/plugin-react';
+// import wasmLoader from 'esbuild-plugin-wasm';
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   server: { open: true },
   preview: { open: true },
+  esbuild: {
+    target: 'es2020',
+    sourcemap: true,
+  },
+  build: {
+    target: 'es2020',
+    sourcemap: true,
+  },
   // css: { modules: { root: '.' } }, // https://github.com/vitejs/vite/issues/3092#issuecomment-915952727
   plugins: [react(), eslintPlugin()],
 });
