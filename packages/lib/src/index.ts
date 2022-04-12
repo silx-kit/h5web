9afc9b7c6312256baad154cc8641db70ec17207e
Axel Bocciarelli <axelboc@gmail.com>
2022-04-12 15:43:39 +0200

    Clarify what is documented and undocumented in lib index

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,11 +1,11 @@
 // Visualizations
 export { default as MatrixVis } from './vis/matrix/MatrixVis';
-export { default as LineVis } from './vis/line/LineVis';
-export { default as HeatmapVis } from './vis/heatmap/HeatmapVis';
-export { default as ScatterVis } from './vis/scatter/ScatterVis';
 export type { MatrixVisProps } from './vis/matrix/MatrixVis';
+export { default as LineVis } from './vis/line/LineVis';
 export type { LineVisProps } from './vis/line/LineVis';
+export { default as HeatmapVis } from './vis/heatmap/HeatmapVis';
 export type { HeatmapVisProps } from './vis/heatmap/HeatmapVis';
+export { default as ScatterVis } from './vis/scatter/ScatterVis';
 export type { ScatterVisProps } from './vis/scatter/ScatterVis';
 
 // Toolbar and controls
@@ -31,15 +31,13 @@ export { default as VisCanvas } from './vis/shared/VisCanvas';
 export type { VisCanvasProps } from './vis/shared/VisCanvas';
 export { default as TooltipMesh } from './vis/shared/TooltipMesh';
 export type { TooltipMeshProps } from './vis/shared/TooltipMesh';
-export { default as VisMesh } from './vis/shared/VisMesh';
+export { default as Html } from './vis/shared/Html';
+export { default as Annotation } from './vis/shared/Annotation';
+
 export { default as ColorBar } from './vis/heatmap/ColorBar';
 export type { ColorBarProps } from './vis/heatmap/ColorBar';
 export { default as HeatmapMesh } from './vis/heatmap/HeatmapMesh';
 export type { HeatmapMeshProps } from './vis/heatmap/HeatmapMesh';
-export { default as DataCurve } from './vis/line/DataCurve';
-export { default as Html } from './vis/shared/Html';
-export { default as Overlay } from './vis/shared/Overlay';
-export { default as Annotation } from './vis/shared/Annotation';
 export { default as TiledHeatmapMesh } from './vis/tiles/TiledHeatmapMesh';
 export type { TiledHeatmapMeshProps } from './vis/tiles/TiledHeatmapMesh';
 export { getLayerSizes, TilesApi } from './vis/tiles/api';
@@ -92,6 +90,8 @@ export { useCanvasEvents } from './interactions/hooks';
 export { INTERPOLATORS } from './vis/heatmap/interpolators';
 export { ScaleType } from '@h5web/shared';
 export { CurveType } from './vis/line/models';
+export { ImageType } from './vis/rgb/models';
+
 export type {
   InteractionInfo,
   ModifierKey,
@@ -126,5 +126,7 @@ export { default as CellWidthInput } from './toolbar/controls/CellWidthInput';
 export { default as RawVis } from './vis/raw/RawVis';
 export { default as ScalarVis } from './vis/scalar/ScalarVis';
 export { default as RgbVis } from './vis/rgb/RgbVis';
+export { default as VisMesh } from './vis/shared/VisMesh';
 export { default as ScatterPoints } from './vis/scatter/ScatterPoints';
-export { ImageType } from './vis/rgb/models';
+export { default as DataCurve } from './vis/line/DataCurve';
+export { default as Overlay } from './vis/shared/Overlay';
