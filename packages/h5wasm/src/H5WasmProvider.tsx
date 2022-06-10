060abcb9985b960d42a62a29dd9aba39faacbf37
Axel Bocciarelli <axelboc@gmail.com>
2022-06-10 14:49:29 +0200

    Fix h5wasm file clean-up in strict mode

--- packages/h5wasm/src/H5WasmProvider.tsx
+++ packages/h5wasm/src/H5WasmProvider.tsx
@@ -1,26 +1,29 @@
 import { DataProvider } from '@h5web/app';
-import type { ReactNode } from 'react';
-import { useEffect, useMemo } from 'react';
+import type { PropsWithChildren } from 'react';
+import { useState, useEffect } from 'react';
 
 import { H5WasmApi } from './h5wasm-api';
 
 interface Props {
   filename: string;
   buffer: ArrayBuffer;
-  children: ReactNode;
 }
 
-function H5WasmProvider(props: Props) {
+function H5WasmProvider(props: PropsWithChildren<Props>) {
   const { filename, buffer, children } = props;
 
-  const api = useMemo(
-    () => new H5WasmApi(filename, buffer),
-    [filename, buffer]
-  );
+  const [api, setApi] = useState<H5WasmApi>();
 
   useEffect(() => {
-    return () => void api.cleanUp();
-  }, [api]);
+    const h5wasmApi = new H5WasmApi(filename, buffer);
+    setApi(h5wasmApi);
+
+    return () => void h5wasmApi.cleanUp();
+  }, [filename, buffer]);
+
+  if (!api) {
+    return null;
+  }
 
   return <DataProvider api={api}>{children}</DataProvider>;
 }
