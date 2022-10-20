b147c02ce8abe4d0e27bf12e9278ac0c6b6f7186
Axel Bocciarelli <axelboc@gmail.com>
2022-10-20 13:50:55 +0200

    Report type-checking errors in development

--- apps/demo/vite.config.js
+++ apps/demo/vite.config.js
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { checker } from 'vite-plugin-checker';
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   server: { open: true },
   preview: { open: true },
-  plugins: [react(), eslintPlugin()],
+  plugins: [react(), eslintPlugin(), checker({ typescript: true })],
 
   // `es2020` required by @h5web/h5wasm for BigInt `123n` notation support
   optimizeDeps: { esbuildOptions: { target: 'es2020' } },
