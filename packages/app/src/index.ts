36df934ea2d38f4a9d74985491f652a7b89365cf
Axel Bocciarelli <axelboc@gmail.com>
2022-10-28 15:54:01 +0200

    Export type of `getExportURL` function

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,3 +1,5 @@
+import type { DataProviderApi } from './providers/api';
+
 export { default as App } from './App';
 
 export { default as MockProvider } from './providers/mock/MockProvider';
@@ -7,6 +9,7 @@ export { default as H5GroveProvider } from './providers/h5grove/H5GroveProvider'
 export { getFeedbackMailto } from './breadcrumbs/utils';
 export type { FeedbackContext } from './breadcrumbs/models';
 export type { ExportFormat, ExportURL } from './providers/models';
+export type GetExportURL = NonNullable<DataProviderApi['getExportURL']>;
 
 // Context
 export { useDataContext } from './providers/DataProvider';
