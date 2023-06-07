96f3883762b48c6d678d29a98671a3cbb5265ff9
Axel Bocciarelli <axelboc@gmail.com>
2023-06-07 13:11:04 +0200

    Increase `waitForStableDom` timeout in E2E specs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,13 @@ const { createConfig } = require('./eslint.shared');
 
 module.exports = createConfig(__dirname, getDependencies(), [
   {
-    files: ['**/*.cy.ts'],
+    files: ['cypress/**/*.ts'],
     rules: {
       'testing-library/await-async-query': 'off', // Cypress has its own way of dealing with asynchronicity
       'testing-library/await-async-utils': 'off', // Cypress has its own way of dealing with asynchronicity
       'testing-library/prefer-screen-queries': 'off', // Cypress provides `cy` object instead of `screen`
       'sonarjs/no-duplicate-string': 'off', // incompatible with Cypress testing syntax
+      'unicorn/numeric-separators-style': 'off', // not supported
     },
   },
 ]);
