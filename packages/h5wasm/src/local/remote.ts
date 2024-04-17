26a524936c6f392be234d82ab46993a3052ab38b
Axel Bocciarelli <axelboc@gmail.com>
2024-04-17 14:37:59 +0200

    Inline web worker used by H5WasmLocalFileProvider

--- packages/h5wasm/src/local/remote.ts
+++ packages/h5wasm/src/local/remote.ts
@@ -2,18 +2,14 @@ import type { Remote } from 'comlink';
 import { wrap } from 'comlink';
 
 import type { H5WasmWorkerAPI } from './worker';
+import LocalWorker from './worker?worker&inline';
 
 let remote: Remote<H5WasmWorkerAPI>;
 
 export function getH5WasmRemote() {
-  if (remote) {
-    return remote;
+  if (!remote) {
+    remote = wrap<H5WasmWorkerAPI>(new LocalWorker());
   }
 
-  const worker = new Worker(new URL('worker.ts', import.meta.url), {
-    type: 'module',
-  });
-
-  remote = wrap<H5WasmWorkerAPI>(worker);
   return remote;
 }
