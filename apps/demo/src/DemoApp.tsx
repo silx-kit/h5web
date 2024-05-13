85bd21bb6f371dc3cfc36851fbb7acb84ebff457
Axel Bocciarelli <axelboc@gmail.com>
2024-05-13 15:49:31 +0200

    Lazy-load H5Wasm demo

--- apps/demo/src/DemoApp.tsx
+++ apps/demo/src/DemoApp.tsx
@@ -1,10 +1,13 @@
+import { lazy, Suspense } from 'react';
 import { Redirect, Route, Switch } from 'wouter';
 
 import H5GroveApp from './H5GroveApp';
-import H5WasmApp from './h5wasm/H5WasmApp';
 import Home from './Home';
 import HsdsApp from './HsdsApp';
 import MockApp from './MockApp';
+
+// Split H5Wasm demo into its own bundle, and load it only when the demo is first visited
+const H5WasmApp = lazy(() => import('./h5wasm/H5WasmApp'));
 
 const query = new URLSearchParams(document.location.search);
 // @ts-expect-error
@@ -12,16 +15,18 @@ window.H5WEB_EXPERIMENTAL = query.has('experimental');
 
 function DemoApp() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/h5grove" component={H5GroveApp} />
-      <Route path="/mock" component={MockApp} />
-      <Route path="/hsds" component={HsdsApp} />
-      <Route path="/h5wasm" component={H5WasmApp} />
-      <Route>
-        <Redirect to="/" replace />
-      </Route>
-    </Switch>
+    <Suspense fallback={null}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/h5grove" component={H5GroveApp} />
+        <Route path="/mock" component={MockApp} />
+        <Route path="/hsds" component={HsdsApp} />
+        <Route path="/h5wasm" component={H5WasmApp} />
+        <Route>
+          <Redirect to="/" replace />
+        </Route>
+      </Switch>
+    </Suspense>
   );
 }
 
