d410294f22d234b75f390aa7e260483bf750a9de
Axel Bocciarelli <axelboc@gmail.com>
2024-05-31 10:37:46 +0200

    Revert "Reduce delay when slicing with h5wasm through viewer config"

--- packages/app/src/providers/hsds/HsdsProvider.tsx
+++ packages/app/src/providers/hsds/HsdsProvider.tsx
@@ -3,12 +3,7 @@ import { useMemo } from 'react';
 
 import type { DataProviderApi } from '../api';
 import DataProvider from '../DataProvider';
-import type { ViewerConfig } from '../models';
 import { HsdsApi } from './hsds-api';
-
-const DEFAULT_VIEWER_CONFIG: ViewerConfig = {
-  slicingTiming: 250,
-};
 
 interface Props {
   url: string;
@@ -16,35 +11,17 @@ interface Props {
   password: string;
   filepath: string;
   getExportURL?: DataProviderApi['getExportURL'];
-  viewerConfig?: Partial<ViewerConfig>;
 }
 
 function HsdsProvider(props: PropsWithChildren<Props>) {
-  const {
-    url,
-    username,
-    password,
-    filepath,
-    getExportURL,
-    viewerConfig,
-    children,
-  } = props;
+  const { url, username, password, filepath, getExportURL, children } = props;
 
   const api = useMemo(
     () => new HsdsApi(url, username, password, filepath, getExportURL),
     [filepath, password, url, username, getExportURL],
   );
 
-  const mergedViewerConfig = useMemo(
-    () => ({ ...DEFAULT_VIEWER_CONFIG, ...viewerConfig }),
-    [viewerConfig],
-  );
-
-  return (
-    <DataProvider api={api} viewerConfig={mergedViewerConfig}>
-      {children}
-    </DataProvider>
-  );
+  return <DataProvider api={api}>{children}</DataProvider>;
 }
 
 export default HsdsProvider;
