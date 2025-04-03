95c003004bd582ef082acf7a0f74d790d79caebe
Axel Bocciarelli <axelboc@gmail.com>
2025-04-03 14:47:22 +0200

    Enable software WebGL in Chrome when running tests in the CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,17 @@ export default defineConfig({
     supportFile: 'cypress/support.ts',
     setupNodeEvents(on) {
       addMatchImageSnapshotPlugin(on);
+
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome') {
+          /* On the CI, since there's no physical GPU, Chrome falls back to
+           * software WebGL (SwiftShader), which is disabled by default.
+           * https://chromestatus.com/feature/5166674414927872 */
+          launchOptions.args.push('--enable-unsafe-swiftshader');
+        }
+
+        return launchOptions;
+      });
     },
   },
   retries: process.env.CYPRESS_TAKE_SNAPSHOTS ? 3 : null,
