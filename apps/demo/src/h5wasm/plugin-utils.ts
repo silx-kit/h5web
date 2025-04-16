7d738f07a7678503379e18d3155a4cf14988936a
Axel Bocciarelli <axelboc@gmail.com>
2025-04-16 10:02:08 +0200

    Upgrade h5wasm-plugins to support JPEG compression

--- apps/demo/src/h5wasm/plugin-utils.ts
+++ apps/demo/src/h5wasm/plugin-utils.ts
@@ -5,9 +5,9 @@ import blosc from 'h5wasm-plugins/plugins/libH5Zblosc.so';
 import blosc2 from 'h5wasm-plugins/plugins/libH5Zblosc2.so';
 import bshuf from 'h5wasm-plugins/plugins/libH5Zbshuf.so';
 import bz2 from 'h5wasm-plugins/plugins/libH5Zbz2.so';
+import jpeg from 'h5wasm-plugins/plugins/libH5Zjpeg.so';
 import lz4 from 'h5wasm-plugins/plugins/libH5Zlz4.so';
 import lzf from 'h5wasm-plugins/plugins/libH5Zlzf.so';
-import szf from 'h5wasm-plugins/plugins/libH5Zszf.so';
 import zfp from 'h5wasm-plugins/plugins/libH5Zzfp.so';
 import zstd from 'h5wasm-plugins/plugins/libH5Zzstd.so';
 
@@ -16,9 +16,9 @@ const PLUGINS: Record<Plugin, string> = {
   [Plugin.Blosc2]: blosc2,
   [Plugin.Bitshuffle]: bshuf,
   [Plugin.BZIP2]: bz2,
+  [Plugin.JPEG]: jpeg,
   [Plugin.LZ4]: lz4,
   [Plugin.LZF]: lzf,
-  [Plugin.SZ]: szf,
   [Plugin.ZFP]: zfp,
   [Plugin.Zstandard]: zstd,
 };
--- packages/h5wasm/src/utils.ts
+++ packages/h5wasm/src/utils.ts
@@ -9,7 +9,7 @@ export const PLUGINS_BY_FILTER_ID: Record<number, Plugin> = {
   32_008: Plugin.Bitshuffle,
   32_013: Plugin.ZFP,
   32_015: Plugin.Zstandard,
-  32_017: Plugin.SZ,
+  32_019: Plugin.JPEG,
   32_026: Plugin.Blosc2,
 };
 
