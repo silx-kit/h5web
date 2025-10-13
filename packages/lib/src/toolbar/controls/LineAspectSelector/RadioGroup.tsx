8b4d7f6727395c79b932e03e336876484fc9d372
Axel Bocciarelli <axelboc@gmail.com>
2025-10-13 15:49:51 +0200

    Tweak spacing inside line aspect selector

--- packages/lib/src/toolbar/controls/LineAspectSelector/RadioGroup.tsx
+++ packages/lib/src/toolbar/controls/LineAspectSelector/RadioGroup.tsx
@@ -1,53 +1,56 @@
 import styles from './RadioGroup.module.css';
 
 interface Props<T extends string> {
-  value: T;
-  onValueChanged: (value: T) => void;
   name: string;
+  label?: string;
   options: T[];
   optionsLabels?: Record<T, string>;
-  label?: string;
   disabled?: boolean;
+  value: T;
+  onValueChanged: (value: T) => void;
 }
 
 function RadioGroup<T extends string>(props: Props<T>) {
   const {
-    value,
-    onValueChanged,
     name,
+    label,
     options,
     optionsLabels,
-    label,
     disabled,
+    value,
+    onValueChanged,
   } = props;
 
   return (
-    <div className={styles.group} role="radiogroup">
-      {label && <span className={styles.groupLabel}>{label}</span>}
-      {options.map((option) => {
-        return (
-          <label
-            id={`${option}-label`}
-            key={option}
-            className={styles.option}
-            htmlFor={option}
-            aria-disabled={disabled}
-          >
-            <input
-              id={option}
-              name={name}
-              type="radio"
-              checked={option === value}
-              onChange={() => onValueChanged(option)}
-              aria-labelledby={`${option}-label`}
-            />
-            <span className={styles.optionText}>
-              {optionsLabels ? optionsLabels[option] : option}
-            </span>
-          </label>
-        );
-      })}
-    </div>
+    <>
+      {label && <span className={styles.label}>{label}</span>}
+
+      <div className={styles.options} role="radiogroup">
+        {options.map((option) => {
+          return (
+            <label
+              id={`${option}-label`}
+              key={option}
+              className={styles.option}
+              htmlFor={option}
+              aria-disabled={disabled}
+            >
+              <input
+                id={option}
+                name={name}
+                type="radio"
+                checked={option === value}
+                onChange={() => onValueChanged(option)}
+                aria-labelledby={`${option}-label`}
+              />
+              <span className={styles.optionText}>
+                {optionsLabels ? optionsLabels[option] : option}
+              </span>
+            </label>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
