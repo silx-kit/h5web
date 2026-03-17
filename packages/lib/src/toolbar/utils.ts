86f48e70adecc0dd4c516ae4ca7fa93b09e57444
Axel Bocciarelli <axelboc@gmail.com>
2026-03-17 11:26:44 +0100

    Disable Ctrl+left click context menu on Mac to fix zoom interactions

--- packages/lib/src/toolbar/utils.ts
+++ packages/lib/src/toolbar/utils.ts
@@ -2,6 +2,10 @@ import { isValidElement, type ReactNode } from 'react';
 
 import { type ReactElementWithKey } from './models';
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/platform
+export const IS_MAC =
+  'navigator' in globalThis && globalThis.navigator.platform.startsWith('Mac'); // eslint-disable-line @typescript-eslint/no-deprecated
+
 export function isValidElementWithKey(
   node: ReactNode,
 ): node is ReactElementWithKey {
