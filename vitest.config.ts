23dc5c207d04c65fe61100dcd2555465c3bfa649
Axel Bocciarelli <axelboc@gmail.com>
2026-04-08 11:53:40 +0200

    Upgrade Vitest and other dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
       'packages/!(app)',
       'packages/app/vitest.{browser,unit}.config.js',
     ],
+    slowTestThreshold: 1000,
     coverage: {
       include: ['packages/*/src/**/*.{ts,tsx}'],
       exclude: ['packages/lib/src/vis/surface', 'packages/lib/src/vis/tiles'], // experimental
