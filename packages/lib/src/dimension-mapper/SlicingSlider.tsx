ae4d0f4929f2480bf7cb09860515a5967383dda0
Axel Bocciarelli <axelboc@gmail.com>
2026-09-21 15:49:26 +0200

    Add optional slicing controls to dimension mapper

--- packages/lib/src/dimension-mapper/SlicingSlider.tsx
+++ packages/lib/src/dimension-mapper/SlicingSlider.tsx
@@ -1,7 +1,9 @@
 import { useMeasure } from '@react-hookz/web';
 import { useState } from 'react';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 import ReactSlider from 'react-slider';
 
+import Btn from '../toolbar/controls/Btn';
 import { useDynamicDebouncedCallback } from './hooks';
 import styles from './SlicingSlider.module.css';
 
@@ -14,16 +16,26 @@ interface Props {
   dimension: number;
   length: number;
   initialValue: number;
+  withControls: boolean;
   isFastSlice?: (value: number) => boolean;
   onChange: (value: number) => void;
 }
 
 function SlicingSlider(props: Props) {
-  const { dimension, length, initialValue, isFastSlice, onChange } = props;
+  const {
+    dimension,
+    length,
+    initialValue,
+    withControls,
+    isFastSlice,
+    onChange,
+  } = props;
 
   if (length <= 0) {
     throw new Error('Expected non-empty dimension');
   }
+
+  const [stride, setStride] = useState(1);
 
   const [value, setValue] = useState(initialValue);
   const onDebouncedChange = useDynamicDebouncedCallback(
@@ -41,43 +53,90 @@ function SlicingSlider(props: Props) {
         D{dimension}
       </span>
       <span className={styles.sublabel}>0:{length - 1}</span>
-      {length > 1 ? (
-        <ReactSlider
-          className={styles.slider}
-          ariaLabelledby={sliderLabelId}
-          min={0}
-          max={length - 1}
-          step={1}
-          marks={
-            containerSize &&
-            containerSize.height / length >= MIN_HEIGHT_PER_MARK
-          }
-          markClassName={styles.mark}
-          orientation="vertical"
-          invert
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-            onDebouncedChange(newValue);
-          }}
-          /* When slicing in E2E tests, `onChange` is called with the old value.
+      {length > 1 && (
+        <>
+          <ReactSlider
+            className={styles.slider}
+            ariaLabelledby={sliderLabelId}
+            min={0}
+            max={length - 1}
+            step={1}
+            marks={
+              containerSize &&
+              containerSize.height / length >= MIN_HEIGHT_PER_MARK
+            }
+            markClassName={styles.mark}
+            orientation="vertical"
+            invert
+            value={value}
+            onChange={(newValue) => {
+              setValue(newValue);
+              onDebouncedChange(newValue);
+            }}
+            /* When slicing in E2E tests, `onChange` is called with the old value.
              The `onChange` event is fired via a `setState` callback in `ReactSlider`:
              https://github.com/zillow/react-slider/blob/master/src/components/ReactSlider/ReactSlider.jsx#L908
              Adding `onAfterChange` fixes the issue for now with react-slider@2.0.4, but not with react-slider@2.0.5+ */
-          onAfterChange={(newValue) => {
-            setValue(newValue);
-            onDebouncedChange(newValue);
-          }}
-          renderThumb={({ key, ...thumbProps }, state) => (
-            <div key={key} {...thumbProps} className={styles.thumb}>
-              <span className={styles.thumbValue}>{state.valueNow}</span>
+            onAfterChange={(newValue) => {
+              setValue(newValue);
+              onDebouncedChange(newValue);
+            }}
+            renderThumb={({ key, ...thumbProps }, state) => (
+              <div key={key} {...thumbProps} className={styles.thumb}>
+                <span className={styles.thumbValue}>{state.valueNow}</span>
+              </div>
+            )}
+            renderTrack={({ key }, { index }) =>
+              index === 0 ? <div key={key} className={styles.track} /> : null
+            }
+          />
+
+          {withControls && (
+            <div className={styles.slicingControls}>
+              <Btn
+                label="Next slice"
+                Icon={FiChevronUp}
+                iconOnly
+                small
+                raised
+                onClick={() => {
+                  const newValue = Math.min(value + stride, length - 1);
+                  setValue(newValue);
+                  onDebouncedChange(newValue);
+                }}
+              />
+              <Btn
+                label="Previous slice"
+                Icon={FiChevronDown}
+                iconOnly
+                small
+                raised
+                onClick={() => {
+                  const newValue = Math.max(value - stride, 0);
+                  setValue(newValue);
+                  onDebouncedChange(newValue);
+                }}
+              />
+              <input
+                type="text"
+                className={styles.strideInput}
+                aria-label={`D${dimension} stride`}
+                defaultValue={stride}
+                onChange={(evt) => {
+                  const newStride = Math.floor(Number(evt.target.value));
+                  if (Number.isFinite(newStride) && newStride > 0) {
+                    setStride(newStride);
+                  }
+                }}
+                onBlur={(evt) => {
+                  const { target } = evt;
+                  target.value = stride.toString(); // overwrite any invalid input on blur
+                }}
+              />
             </div>
           )}
-          renderTrack={({ key }, { index }) =>
-            index === 0 ? <div key={key} className={styles.track} /> : null
-          }
-        />
-      ) : null}
+        </>
+      )}
     </div>
   );
 }
